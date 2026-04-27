eff98a383c800f7dc05d6d8610ac59a9fef625c9
BlahutovaTerez <blahte64@osu.cz>
2026-04-27 14:55:11 +0200

    Create OrderRepository interface for order management

---
+++ backend/src/main/java/com/eshop/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.eshop.repository;
+
+import com.eshop.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    // Zde můžeš později přidat hledání podle čísla objednávky
+}
