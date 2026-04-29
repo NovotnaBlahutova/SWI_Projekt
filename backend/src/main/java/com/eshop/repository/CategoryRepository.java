64c8be35842857b0c384a55f4c752089244b0cf4
BlahutovaTerez <blahte64@osu.cz>
2026-04-29 11:19:09 +0200

    Fix method name in CategoryRepository interface

--- backend/src/main/java/com/eshop/repository/CategoryRepository.java
+++ backend/src/main/java/com/eshop/repository/CategoryRepository.java
@@ -6,6 +6,6 @@ import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findBySlug(String slug);
-    // TADY BYLA CHYBA - musí to být 'Nazev', ne 'Name'
+    
     boolean existsByNazev(String nazev);
 }
