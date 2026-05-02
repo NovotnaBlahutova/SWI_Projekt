7ef3b48d43f458ddb3a89caa9c76d22e00b3cd1e
adela <novoad89@osu.cz>
2026-05-02 16:13:55 +0200

    úprava pro propojení s backendem

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,2 +1,2 @@
 // frontend/src/apiConfig.js
-export const API_BASE_URL = "https://organic-system-wr6497xqq6w4255v7-8080.app.github.dev/api";
+export const API_BASE_URL = "http://localhost:8080/api";
